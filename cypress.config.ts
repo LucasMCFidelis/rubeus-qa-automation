d92cc7b9e4bee1eb58e7657379043e00d19d2125
Lucas Fidelis <lucasm241301@gmail.com>
2026-03-08 10:39:49 -0300

    chore: add cypress-tag config and Tag enum definition

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "cypress";
+import { tagify } from "cypress-tags";
 
 export default defineConfig({
   e2e: {
     baseUrl: "https://qualidade.apprbs.com.br",
     supportFile: "cypress/support/e2e.ts",
     specPattern: "cypress/e2e/**/*.cy.ts",
+    setupNodeEvents(on, config) {
+      on("file:preprocessor", tagify(config));
+    },
   },
 });
---
+++ cypress/support/enums/tag.ts
@@ -0,0 +1,6 @@
+export enum Tag {
+  'WIP' = 0,
+  'REGRESSION' = 1,
+  'SMOKE' = 2,
+  'FEATURE' = 3,
+}
